5ede1c34b9ff38233e679c799df639dd17f79eee
jaycehan <jaycehan@tencent.com>
2024-08-16 17:26:09 +0800

    Initial Commit

---
+++ JCRookieRecorder/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  JCRookieRecorder
+//
+//  Created by jaycehan on 2024/8/16.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
